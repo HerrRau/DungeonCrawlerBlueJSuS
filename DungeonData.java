1bce03daf498de96f80f0f13dd428b5765ffc92c
HerrRau <github@herr-rau.de>
2025-01-21 16:53:11 +0100

    Model und Data geändert

--- DungeonData.java
+++ DungeonData.java
@@ -2,14 +2,33 @@ import crawlergame.*;
 
 public abstract class DungeonData
 {
-    public abstract char[][] gibLevel();
-    public abstract int gibStartX();
-    public abstract int gibStartY();
-    public abstract char gibStartFacing();
-    public abstract Gegenstand gibStandardgegenstand(char c);
-    
+    DungeonModel model;
+
+    public void setzeModel(DungeonModel m) { 
+        model =m; 
+    }
+
     public void zeichneLevel() {
         ViewPrinter printer = new ViewPrinter();
-        printer.zeigeBewegungDungeon(gibLevel(), gibStartX(), gibStartY(), gibStartFacing());
+        if (model != null) {
+            model.begin();
+        } else {
+             printer.zeigeBewegungDungeon(gibLevel(), gibStartX(), gibStartY(), gibStartFacing());
+        }
     }
+
+    public void ergaenzeLevel()  {
+
+    }
+
+    public abstract char[][] gibLevel();
+
+    public abstract int gibStartX();
+
+    public abstract int gibStartY();
+
+    public abstract char gibStartFacing();
+
+    public abstract Gegenstand gibStandardgegenstand(char c);
+
 }
--- DungeonModel.java
+++ DungeonModel.java
@@ -2,16 +2,17 @@ import crawlergame.*;
 
 public class DungeonModel extends DungeonModelAbstract
 {
-    private DungeonData dungeonData;
+    DungeonData dungeonData;
 
     public DungeonModel(DungeonData d) {
         dungeonData = d;
+        dungeonData.setzeModel(this);
         setzeLevel();
     }
     
     public void setzeLevel() {
         super.setzeLevel(dungeonData.gibLevel(), dungeonData.gibStartX(), dungeonData.gibStartY(), dungeonData.gibStartFacing());
-        ergaenzeLevel();
+        dungeonData.ergaenzeLevel();
     }
 
     // used by DungeonModelAbstract and JPanel
@@ -20,8 +21,5 @@ public class DungeonModel extends DungeonModelAbstract
         return dungeonData.gibStandardgegenstand(c);
     }
     
-    public void ergaenzeLevel() {
-        
-    }
 
 }
