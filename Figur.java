8d5d9fa3a87d9c4949086e7fcf6d346520c09ece
HerrRau <github@herr-rau.de>
2025-01-28 15:16:30 +0100

    ix

--- Figur.java
+++ Figur.java
@@ -3,8 +3,22 @@ import crawlergame.*;
 public class Figur extends ModelSender 
 {
     private Waffe waffe;
+    private int staerke;
 
     public Figur() {
+        staerke = 10;
+    }
+
+    public Figur(int i) {
+        staerke = i;
+    }
+
+    public void setzeStaerke(int i) {
+        staerke =i;
+    }
+
+    public int gibStaerke()  {
+        return staerke;
     }
 
     public void setzeWaffe(Waffe w) {
--- Waffe.java
+++ Waffe.java
@@ -1,11 +1,23 @@
 public class Waffe extends Gegenstand
 {
+    int angriff;    
+
     public Waffe()
     {
+        angriff = 5;
+    }
+
+    public void setzeAngriff(int i) {
+        angriff =i;
+    }
+
+    public int gibAngriff()  {
+        return angriff;
     }
 
     public char gibKuerzel() {
         return 'w';
     }
+
 
 }
